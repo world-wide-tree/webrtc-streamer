58ab2461dd1f895bbbebd1ef8c030fcdb4628c17
Ali-0211-TR <xgamexarkon@gmail.com>
2024-06-03 01:58:13 +0500

    Worked. Time of testing...

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use lazy_static::lazy_static;
 use opencv::{core::{self, Vector}, highgui::{self, wait_key}, imgcodecs::imencode_def, prelude::*, videoio::{self, VideoCapture}};
 use reqwest::{Body, Client, Request};
 use tokio::sync::{broadcast::channel, mpsc, Mutex};
-use webrtc::{api::{interceptor_registry::register_default_interceptors, media_engine::MediaEngine, APIBuilder}, data_channel::{data_channel_message::DataChannelMessage, RTCDataChannel}, ice_transport::{ice_candidate::RTCIceCandidate, ice_server::RTCIceServer}, interceptor::registry::Registry, peer_connection::{configuration::RTCConfiguration, math_rand_alpha, offer_answer_options::RTCAnswerOptions, peer_connection_state::RTCPeerConnectionState, sdp::session_description::RTCSessionDescription, RTCPeerConnection}};
+use webrtc::{api::{interceptor_registry::register_default_interceptors, media_engine::MediaEngine, APIBuilder}, data_channel::{data_channel_message::DataChannelMessage, data_channel_state::RTCDataChannelState, RTCDataChannel}, ice_transport::{ice_candidate::RTCIceCandidate, ice_server::RTCIceServer}, interceptor::registry::Registry, peer_connection::{configuration::RTCConfiguration, math_rand_alpha, offer_answer_options::RTCAnswerOptions, peer_connection_state::RTCPeerConnectionState, sdp::session_description::RTCSessionDescription, RTCPeerConnection}};
 
 
 lazy_static! {
@@ -89,49 +89,62 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
         Box::pin(async {})
     }));
-    println!("safdsdf");
-    let offer = loop {
+
+    // creating offer...
+    let offer = peer_connection.create_offer(None).await?;
+    
+    // send offer to SignalService...
+    let r = clnt.post("http://0.0.0.0:3030/offer/0").body(serde_json::to_string(&offer)?).send().await?;
+    
+    // Set local description...
+    peer_connection.set_local_description(offer).await?;
+    
+    // await answer from client
+    let answer = loop {
         let r = clnt.get("http://0.0.0.0:3030/signal/0").send().await?;
         if r.status().is_success(){
             let sdp: RTCSessionDescription = r.json().await?;
             break sdp;
-        } else {
-            println!("1");
         }
 
         tokio::time::sleep(Duration::from_secs(1)).await;
     };
 
-    peer_connection.set_remote_description(offer).await?;
-    // Create Answer
-    let answer = peer_connection.create_answer(None).await?;
+    // Set remote description
+    peer_connection.set_remote_description(answer).await?;
     println!("safdsdf");
-
+    
     // Sent SDP to SignalService
-    let r = clnt.post("http://0.0.0.0:3030/answer/0").body(serde_json::to_string(&answer)?).send().await?;
+    //let r = clnt.post("http://0.0.0.0:3030/answer/0").body(serde_json::to_string(&answer)?).send().await?;
+    
 
-
-    peer_connection.set_local_description(answer).await?;
+    // peer_connection.set_local_description(answer).await?;
 
     let dc = peer_connection.create_data_channel("VideoStreamer", None).await?;
 
     loop {
         if let Ok(frm_buf) = tr.try_recv(){
             let data = Bytes::from(frm_buf.to_vec());
-            if let Err(e) = dc.send(&data).await{
-                println!("{e}");
+            match dc.ready_state(){
+                RTCDataChannelState::Open => {
+                    if let Err(e) = dc.send(&data).await{
+                        println!("{e}");
+                    }
+                },
+                RTCDataChannelState::Closed => break,
+                _ => continue,
             }
         }
 
     }
-    tokio::select! {
-        _ = done_rx.recv() => {
-            println!("received done signal!");
-        }
-        _ = tokio::signal::ctrl_c() => {
-            println!();
-        }
-    };
+    // tokio::select! {
+    //     _ = done_rx.recv() => {
+    //         println!("received done signal!");
+    //     }
+    //     _ = tokio::signal::ctrl_c() => {
+    //         println!();
+    //     }
+    // };
     peer_connection.close().await?;
 
     Ok(())
